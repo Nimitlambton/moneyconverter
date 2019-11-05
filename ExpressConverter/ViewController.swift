8e6921d88b7dcc0ae0fc2f17e809b21086faec53
MacStudent <macstudent@126-iMac01.local>
2019-11-04 19:25:09 -0500

    basic

---
+++ ExpressConverter/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  ExpressConverter
+//
+//  Created by MacStudent on 2019-11-04.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+   var one : Double = 0.0
+    var Two : Double = 0.0
+    
+    
+    
+    @IBOutlet weak var currencyOne: UITextField!
+    
+    
+    @IBOutlet weak var currencyTwo: UITextField!
+    
+    
+    
+    @IBAction func convertbtn(_ sender: Any) {
+        
+        
+        one = Double(currencyOne.text! ) ?? 0
+             
+              
+              
+              //two = Double(currencyTwo.text!) ?? 0
+              
+              
+        let convertedAmount = one * 56
+              
+              currencyTwo.text = String(convertedAmount)
+              
+        
+    }
+    
+    
+    
+    
+}
+
